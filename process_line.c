08296a0f1a75111d2738d62a988d8eb88ca2fa51
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-05-09 17:29:46 +0000

    created process_line

---
+++ process_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * process_line - tokenizes the line into commands and
+ *                executes each one
+ * @line: the line to process
+ * @args: list of arguments
+ * Return: 0 on success, 1 on failure
+ */
+
+int process_line(char *line, char *args[])
+{
+	int num_tokens = 0;
+	char *token;
+
+	/* Tokenize the line into commands and store them in cmds */
+	token = strtok(line, " \n");
+	while (token)
+	{
+		if(line !=NULL)
+		{args[num_tokens] = token;
+		token = strtok(NULL, " \n");
+		num_tokens++;
+		}
+	}
+	args[num_tokens] = NULL;
+	return (num_tokens);
+}
