4574744cd3b08f5cb361ee3c7d1afc53ddf17191
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-05-09 14:38:32 +0000

    update main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - a simple shell that can run commands
+ * @argc: number of arguments
+ * @argv: list of arguments
+ * Return: 0 on success, 1 on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	char *input_buffer;
+	(void)argc;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			write(STDOUT_FILENO, "Xshell> ", 8);
+		}
+		input_buffer = getline();
+		execute_cmd(input_buffer, argv);
+		free(input_buffer);
+	}
+	return (0);
+}
