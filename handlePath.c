096fd372f38424a169bd00aa21c827bad83c3c4a
fidanhuseynova04 <fidanhsynv04@gmail.com>
2024-05-09 15:52:56 +0000

    handlePath.c

---
+++ handlePath.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * slash_checker - check the file nmae that it begins with slash or not
+ * @str: str to be checked
+ * Return: 0 in success or 1 in failure
+ **/
+int slash_checker(const char *str)
+{
+	if (str != NULL)
+	{
+		return (1);
+	}
+	return (0);
+}
