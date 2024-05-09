0eb2d89ab1e62070325d9c92d7b73d7828060c5e
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-05-09 16:48:13 +0000

    create handle_builtin

---
+++ handle_builtin.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* handle_builtin_commands - handel builtin commands
+* @args: list of arguments
+* @status: status code
+* Return: returns 0 on success 1 else
+*/
+
+int handle_builtin_commands(char *cmd, char **args, int status)
+{
+
+	if (strcmp(args[0], "exit") == 0)
+	{
+		free(cmd);
+		shell_exit(status);
+	}
+}
