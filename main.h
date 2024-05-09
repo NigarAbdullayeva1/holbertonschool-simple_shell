3b0a803d9acbf3f54e9473fedd1717826c3c51fe
NigarAbdullayeva1 <8776@holbertonstudents.com>
2024-05-09 10:29:58 +0000

    create main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+extern char **environ;
+
+#endif
